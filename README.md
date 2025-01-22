f9d3fb595a6bd5d4de3ce4ef7749c6ab021a5d64
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-01-22 16:40:23 +0100

    Day 2 of building, second push.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
+# Genomac Holdings
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A website that generates certificates.
+You just have to enter the name of the graduant and your customized certificate is ready...
+In just the click of a button!
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import sign1 from '../assets/sign1.png';
-import sign2 from '../assets/sign2.png';
+// import sign2 from '../assets/sign2.png';
 import award from '../assets/award.png';
 import genomac from '../assets/genomac.png';
 import barcode from '../assets/barcode.png';
@@ -20,14 +20,54 @@ const Certificate = () => {
   return (
   <>
     <div className="flex items-center justify-center min-h-screen">
-      <div id="certificateContent" className="flex border-2 lg:h-[600px] lg:w-[1100px] mx-auto text-center">
-        <div className="bg-purple-900 lg:w-[210px] lg:h-[600px]">
-          <div className="flex justify-normal " >
-            <img src={genomac} alt="logo" className="w-20" />
-            <p>
-              <img src={gihub} alt="logo2" className="w-20" />
+      <div id="certificateContent" className="flex border-2 lg:h-[650px] lg:w-[1100px] mx-auto text-center">
+        <div className="bg-purple-900 lg:w-[210px] lg:h-[648px]">
+          <div className="flex absolute pt-2" >
+            <div className="p-5 relative top-1 ml-4" >
+              <img src={genomac} alt="logo" className="w-20" />
+            </div>
+            <div className="p-2 relative right-[65px] " >
+              <img src={gihub} alt="logo2" className="w-28" />
+            </div>
+          </div>
+          <div className="mt-[100px] " >
+            <p className="text-white text-[13px] " >
+              GENOMAC INSTITUTE INC
+            </p>
+            <p className="text-yellow-500 font-thin text-[8px]  " >
+              ...discovering new things, imporving life
             </p>
           </div>
+
+
+          <div className="text-white text-[10px] m-2 p-2 bg-purple-950 rounded-xl text-left mt-[100px] " >
+            <p>
+              This certificate is issued by Genomac Institute Inc. a registered research institution in the United States of America.
+            </p>
+            <p>
+              Registration Number : 3844801
+            </p>
+          </div>
+
+          <div className="text-[10px] m-2 p-2 bg-purple-950 rounded-xl text-left " >
+            <div className="bg-white w-fit p-1 m-2 " >
+              USA Office Address
+            </div>
+            <p className="text-white" >
+              The corporation&apos;s registered off in the state of Delaware is located at 16192 coastal highway, lewes, Delware 19958, county of Sussex
+            </p>
+          </div>
+
+          <div className="text-[10px] m-2 p-2 bg-purple-950 rounded-xl text-left " >
+            <div className="bg-white w-fit p-1 m-2 " >
+              NIG Office Address
+            </div>
+            <p className="text-white" >
+              Genomac Holdings, beside Alari Akata Filling Station, Unde-G, Ogbomoso, Oyo State, Nigeria.
+            </p>
+          </div>
+
+
         </div>
         <div className="lg:w-[800px]">
           <div className="bg-white lg:w-[850px] lg:h-[600px] lg:ml-4 lg:p-10">
@@ -43,7 +83,7 @@ const Certificate = () => {
               {name}
             </div>
 
-            <p className="py-3 pr-1 text-center">
+            <p className="py-4 pr-1 text-center font-medium ">
               For Successfully Participating in the International Virtual Bio-coding Workshop on Mastering Python and R for Basic to Advance Genomics and Bioinformatics with NGS Research Applications Organized by Genomac Institute Inc.
             </p>
 
@@ -51,30 +91,39 @@ const Certificate = () => {
               4TH NOVEMBER - 6TH DECEMBER 2024
             </p>
 
-            <div className="flex pt-10 relative">
-              <p className="relative -left-20 bottom-24">
-                <img src={barcode} alt="Barcode" className="w-[400px]"/>
-              </p>
+            <div className="absolute flex bottom- " >
+              <div className="w-[400px] -ml-[160px] -mb-[200px] " >
+                <img src={barcode} alt="barcode" />
+              </div>
 
-              <p className="relative -left-40 bottom-[72px] w-[295px]">
-                <div className="border-b-2 border-blue-700">
-                  <img src={sign1} alt="signature" className="w-[200px] h-[200px] -mb-16"/>
+              <div className="w-[300px] text-left relative -ml-[120px] " >
+                <div className="border-b-2 border-blue-700 " >
+                  <img src={sign1} alt="signature" className="-mb-16 " />
                 </div>
-                <p className="text-left">Oluwaseyi Abraham Olawale <br/> Founder & CEO of Genomac Institute Inc. </p>
-              </p>
 
-              <p className="relative -left-20 bottom-[8px]">
-                <div className="border-b-2 border-blue-700">
-                  <img src={sign1} alt="signature" className="w-[100px]"/>
+                <div>
+                  <p>Oluwaseyi Abraham Olawale</p>
+                  <p>Founder & CEO of Genomac Institute INC.</p>
                 </div>
-                <p>Aderinto Abraham <br/> Director of G-iHub </p>
-              </p>
+              </div>
 
-              <p className="relative" >
-                <img src={award} alt="award" className="w-[400px]"/>
-              </p>
 
+              <div className="w-[140px] text-left relative ml-20 mt-[75px] " >
+                <div className="border-b-2 border-blue-700 " >
+                  <img src={sign1} alt="signature" className="-mb-6 " />
+                </div>
+
+                <div>
+                  <p>Aderinto Abraham </p>
+                  <p>Director of G-iHub</p>
+                </div>
+              </div>
+
+              <div className="relative w-[400px] -ml-[150px] mt-[5px] " >
+                <img src={award} alt="" />
+              </div>
             </div>
+
           </div>
         </div>
       </div>
