16bedd0e447b85f54ff1982aedde145d08ed5a3b
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-01-27 13:16:58 +0100

    I have lost count of the days now.

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -5,23 +5,91 @@ import award from '../assets/award.png';
 import genomac from '../assets/genomac.png';
 import barcode from '../assets/barcode.png';
 import gihub from '../assets/gihub.png';
+import usa from '../assets/usa.png';
+import nig from '../assets/nig.png';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 const Certificate = () => {
 
   const [name, setName] = useState('');
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const storedName = localStorage.getItem('name');
     if (storedName) {
     setName(storedName);
     }
+
+    const downloadImageButton = document.getElementById('downloadImage');
+    const handleDownloadImage = () => {
+      const certificateContent = document.getElementById('certificateContent');
+      if (certificateContent) {
+        html2canvas(certificateContent).then(canvas => {
+          let link = document.createElement('a');
+          link.download = 'certificate.png';
+          link.href = canvas.toDataURL();
+          link.click();
+        });
+      }
+    };
+
+    if (downloadImageButton) {
+      downloadImageButton.addEventListener('click', handleDownloadImage);
+    }
+
+    const downloadPDFButton = document.getElementById('downloadPDF');
+    const handleDownloadPDF = () => {
+      const certificateContent = document.getElementById('certificateContent');
+      if (certificateContent) {
+        html2canvas(certificateContent).then(canvas => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF('landscape');
+          pdf.addImage(imgData, 'PNG', 10, 10);
+          pdf.save('certificate.pdf');
+        });
+      }
+    };
+
+    if (downloadPDFButton) {
+      downloadPDFButton.addEventListener('click', handleDownloadPDF);
+    }
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      if (downloadImageButton) {
+        downloadImageButton.removeEventListener('click', handleDownloadImage);
+      }
+      if (downloadPDFButton) {
+        downloadPDFButton.removeEventListener('click', handleDownloadPDF);
+      }
+    };
+
   }, []);
+
+  if (isMobile) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center p-4 bg-white shadow-lg rounded-lg">
+          <h1 className="text-2xl font-bold">Please Switch to Desktop Mode</h1>
+          <p className="mt-2">This page is best viewed on a desktop device.</p>
+        </div>
+      </div>
+    );
+  }
+
 
   return (
   <>
-    <div className="flex items-center justify-center min-h-screen">
-      <div id="certificateContent" className="flex border-2 lg:h-[650px] lg:w-[1100px] mx-auto text-center">
-        <div className="bg-purple-900 lg:w-[210px] lg:h-[648px]">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div id="certificateContent" className="flex border-2 lg:h-[650px] lg:w-[1100px] mx-auto text-center ">
+        <div className="bg-purple-900 lg:w-[210px] lg:h-[648px] ">
           <div className="flex absolute pt-2" >
             <div className="p-5 relative top-1 ml-4" >
               <img src={genomac} alt="logo" className="w-20" />
@@ -35,7 +103,7 @@ const Certificate = () => {
               GENOMAC INSTITUTE INC
             </p>
             <p className="text-yellow-500 font-thin text-[8px]  " >
-              ...discovering new things, imporving life
+              ...discovering new things, improving life
             </p>
           </div>
 
@@ -50,16 +118,18 @@ const Certificate = () => {
           </div>
 
           <div className="text-[10px] m-2 p-2 bg-purple-950 rounded-xl text-left " >
-            <div className="bg-white w-fit p-1 m-2 " >
+            <div className="bg-white w-fit p-1 m-2 mx-auto flex items-center font-medium" >
+              <span className="-ml-5 mr-1" ><img src={usa} alt="usa" className="w-6" /></span>
               USA Office Address
             </div>
             <p className="text-white" >
-              The corporation&apos;s registered off in the state of Delaware is located at 16192 coastal highway, lewes, Delware 19958, county of Sussex
+              The corporation&apos;s registered office in the state of Delaware is located at 16192 coastal highway, lewes, Delware 19958, county of Sussex
             </p>
           </div>
 
           <div className="text-[10px] m-2 p-2 bg-purple-950 rounded-xl text-left " >
-            <div className="bg-white w-fit p-1 m-2 " >
+            <div className="bg-white w-fit p-1 m-2 flex items-center mx-auto font-medium " >
+              <span className="-ml-5 mr-1" ><img src={nig} alt="nig" className="w-6" /></span>
               NIG Office Address
             </div>
             <p className="text-white" >
@@ -69,6 +139,7 @@ const Certificate = () => {
 
 
         </div>
+
         <div className="lg:w-[800px]">
           <div className="bg-white lg:w-[850px] lg:h-[600px] lg:ml-4 lg:p-10">
             <div className="p-5 bg-purple-900 text-white text-4xl tracking-widest ">
@@ -79,7 +150,7 @@ const Certificate = () => {
               This Certificate is Presented to:
             </div>
 
-            <div id="name" className="border-b-4 border-purple-900 pb-1 text-purple-900 lg:mx-[100px] lg:mt-[70px] text-5xl">
+            <div id="name" className="border-b-4 border-purple-900 pb-1 text-purple-900 mx-[70px] mt-[70px] text-5xl">
               {name}
             </div>
 
@@ -96,7 +167,7 @@ const Certificate = () => {
                 <img src={barcode} alt="barcode" />
               </div>
 
-              <div className="w-[300px] text-left relative -ml-[120px] " >
+              <div className="w-[300px] text-left  -ml-[120px] " >
                 <div className="border-b-2 border-blue-700 " >
                   <img src={sign1} alt="signature" className="-mb-16 " />
                 </div>
@@ -108,7 +179,7 @@ const Certificate = () => {
               </div>
 
 
-              <div className="w-[140px] text-left relative ml-20 mt-[75px] " >
+              <div className="w-[140px] text-left  ml-20 mt-[75px] " >
                 <div className="border-b-2 border-blue-700 " >
                   <img src={sign1} alt="signature" className="-mb-6 " />
                 </div>
@@ -119,7 +190,7 @@ const Certificate = () => {
                 </div>
               </div>
 
-              <div className="relative w-[400px] -ml-[150px] mt-[5px] " >
+              <div className=" w-[400px] -ml-[150px] mt-[5px] " >
                 <img src={award} alt="" />
               </div>
             </div>
@@ -127,9 +198,11 @@ const Certificate = () => {
           </div>
         </div>
       </div>
-      <div className="mt-20 hidden">
-        <button id="downloadImage" className="mr-4">Download as Image</button>
-        <button id="downloadPDF">Download as PDF</button>
+
+      
+      <div className="mt-10 flex  text-white space-x-3 ">
+        <button id="downloadImage" className="bg-purple-900 py-2 px-5 rounded-2xl hover:bg-black ">Download as Image</button>
+        <button id="downloadPDF" className="bg-purple-900 py-2 px-5 rounded-2xl hover:bg-purple-800 " >Download as PDF</button>
       </div>
     </div>
   </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,14 +17,16 @@ const Home = () => {
   return (
     <>
     
-      <div className="w-full max-w-lg lg:w-fit lg:h-auto bg-white p-6 shadow-2xl rounded-xl">
-          <h1 className="text-center text-3xl text-white p-5 rounded-t-xl bg-purple-950">Certificate Generator</h1>
-          <form id="certificateForm" className="flex flex-col p-5" onSubmit={handleSubmit}>
-              <div className="py-2">
-                  <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} name="name" placeholder="Name" required className="w-full rounded-xl p-2 pl-5 outline outline-1" />
-              </div>
-              <button type="submit" className="bg-purple-950 text-white p-3 my-5 rounded-full w-full hover:bg-purple-900">Generate Certificate</button>
-          </form>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-lg lg:w-fit lg:h-auto bg-white p-6 shadow-2xl rounded-xl">
+            <h1 className="text-center text-3xl text-white p-5 rounded-t-xl bg-purple-950">Certificate Generator</h1>
+            <form id="certificateForm" className="flex flex-col p-5" onSubmit={handleSubmit}>
+                <div className="py-2">
+                    <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} name="name" placeholder="Name" required className="w-full rounded-xl p-2 pl-5 outline outline-1" />
+                </div>
+                <button type="submit" className="bg-purple-950 text-white p-3 my-5 rounded-full w-full hover:bg-purple-900">Generate Certificate</button>
+            </form>
+        </div>
       </div>
     
     
