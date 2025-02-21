fe2aa8e8dc674399daa88d7a6be22f27a22f652e
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-02-21 11:01:48 +0100

    Final, Institute 04.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,13 +41,21 @@ const Home = () => {
             <div className="py-2 ml-1 relative">
               <select name="course" id="course" value={formData.course} onChange={handleChange} placeholder="Course" className='w-full h-10 rounded-xl pl-3 outline outline-1 appearance-none hover:outline-purple-800 transition-colors duration-200 ' >
                 <option value="" disabled>Course</option>
-                <option value="Cancer Genomics" className=' appearance-none hover:bg-' >Cancer Genomics</option>
-                <option value="Plant Genomics">Plant Genomics</option>
-                <option value="Virulence Genomics">Virulence Genomics</option>
-                <option value="Infectious Disease">Infectious Disease</option>
-                <option value="Nutri Genomics">Nutri Genomics</option>
-                <option value="Anti Bacteria R&D">Anti Bacteria R&D</option>
-                <option value="Anti Cancer R&D">Anti Cancer R&D</option>
+
+                <option value="Cancer Genomics: To conduct comprehensive genomic profiling and variant analysis across various cancer cell lines." className=' appearance-none hover:bg-' >Cancer Genomics</option>
+
+                <option value="Plant Genomics: To investigate the genetic variants in plants that contribute to stress resistance, aiming to understand the molecular mechanisms and evolutionary adaptations that enhance plant resilience under environmental stress conditions.">Plant Genomics</option>
+
+                <option value="Virulence Genomics: To study the distribution, prevalence, and genomic characteristics of virulence factors in clinical pathogens across diverse geographical regions.">Virulence Genomics</option>
+
+                <option value="Infectious Disease: Understanding the genetic mechanisms underlying multidrug resistance pathogens and comparing strains from clinical, food, or environmental sources.">Infectious Disease</option>
+
+                <option value="Nutri Genomics: To explore the genomic foundations of probiotic strains, identifying and understanding the functional genes associated to their nutritional and industrial benefits.">Nutri Genomics</option>
+
+                <option value="Anti Bacteria R&D: To innovate a comprehensive methodology by leveraging multi-omics datasets and advanced machine learning algorithms to systematically identify, enhance, and develop new therapeutic antibacterial peptides derived from various natural sources.">Anti Bacteria R&D</option>
+
+                <option value="Anti Cancer R&D: To systematically identify and utilize biosynthetic genes from natural sources, employing multi-omics data and machine learning techniques to develop peptide-based anti-cancer therapeutics to improve precision and efficacy in cancer treatment.">Anti Cancer R&D</option>
+
               </select>
             </div>
 
