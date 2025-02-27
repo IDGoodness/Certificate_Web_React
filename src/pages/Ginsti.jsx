be773df51a821af079a6184c366c924a30a5d092
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-02-27 14:57:31 +0100

    Institute free course 01.

--- src/pages/Ginsti.jsx
+++ src/pages/Ginsti.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import logo from '../assets/ginsti.png';
-import glogo from '../assets/genomac.png';
+// import glogo from '../assets/genomac.png';
 import sign1 from '../assets/sign1.png';
 import sign2 from '../assets/signInsti.png';
 import award from '../assets/award.png';
@@ -63,104 +63,111 @@ const Ginsti = () => {
     }, []);
 
     return (
-        <>
-            <div className="min-w-[1000px] flex justify-center items-center min-h-screen ">
-                <div ref={ref} className="flex flex-col justify-center items-center bg-white ">
-                    <div className="relative w-[1000px] h-[600px] border-[20px] m-10 border-purple-500 flex ">
-                        <div className="flex flex-col mx-auto">
-                            <div className="flex text-center mx-auto">
-                                <p className="mr-2">
-                                    <img src={glogo} alt="logo" className="w-[80px] " />
-                                </p>
-                                <p className="w-[1px] h-[50px] bg-purple-600 mt-4 "></p>
-                                <p className="ml-4 w-[80px] ">
-                                    <img src={logo} alt="logo" className=" " />
-                                </p>
-                            </div>
-
-                            <div className="text-center mx-auto">
-                                <p className="uppercase font-semibold text-3xl ">
-                                    certificate of learning
-                                </p>
-                                <p className="text-center italic font-bold ">
-                                    this is to certify that:
-                                </p>
-                            </div>
-
-                            <div className=" text-center mx-auto pt-5 pb-10 w-[1000px] h-[200px] mt-5 ">
-                                <p className="text-3xl font-semibold border-b-2 mx-[300px] pb-2 mb-1 border-purple-800 border-dashed "> {formData.name} </p>
-                                <p>
-                                    for demonstrating active learning in the short course on
-                                </p>
-                                <p className='font-bold text-purple-800 capitalize text-lg py-3 ' >
-                                    Applied functional genomics and bioinformatics
-                                </p>
-                                <p className="mx-20 ">
-                                    This fully funded short course covers an in-depth hands-on
-                                    exploration of functional genomics focusing on genome
-                                    annotation and functional discovery across plant, animal and
-                                    human genomes by leveraging bioinformatics tools for
-                                    comprehensive genomic analysis, enabling extraction of
-                                    meaningful insights from raw genomic data.
-                                </p>
-                                <p className="font-bold uppercase"> {formattedDate} </p>
-                            </div>
-
-                            <div className="flex justify-between mx-32 ">
-                                <div className=" ">
-                                    <p className="border-b-2 border-dashed border-purple-800 w-[200px] ">
-                                        <img
-                                            src={sign1}
-                                            alt="signature"
-                                            className="w-[200px] h-[150px] -mb-10 "
-                                        />
-                                    </p>
-                                    <p className="text-base font-semibold">
-                                        Oluwaseyi Abraham Olawale
-                                    </p>
-                                    <p className="text-xs font-medium">
-                                        Founder & CEO of Genomac Holdings.
-                                    </p>
-                                </div>
-
-                                <div className="w-[400px] h-auto -mt-[30px] -ml-[400px] -mr-[320px] z-10">
-                                    <img src={award} alt="award" />
-                                </div>
-
-                                <div className="mt-2 ">
-                                    <p className="border-b-2 border-dashed border-purple-800 w-52 ">
-                                        <img
-                                            src={sign2}
-                                            alt="signature"
-                                            className="w-[200px] h-[150px] -mb-12 "
-                                        />
-                                    </p>
-                                    <p className="text-base font-semibold">
-                                        Oluwaseun Oyekunle Agboola
-                                    </p>
-                                    <p className="text-xs font-medium">
-                                        Director, Genomac Institute INC.
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div className="w-[150px] absolute top-8 left-14 ">
-                                <img src={award1} alt="award" />
-                            </div>
-                        </div>
-                    </div>
+      <>
+        <div className="min-w-[1000px] flex justify-center items-center min-h-screen ">
+          <div
+            ref={ref}
+            className="flex flex-col justify-center items-center bg-white "
+          >
+            <div className="relative w-[1000px] h-[600px] border-[20px] m-10 border-purple-500 flex ">
+              <div className="flex flex-col mx-auto">
+                <div className="flex text-center mx-auto pl-20 ">
+                  <p className="mr-2 ">
+                    <img src={logo} alt="logo" className="w-[80px] " />
+                  </p>
+                  <p className="w-[1px] h-[50px] bg-purple-600 mt-4 "></p>
+                  <p className="mt-5 pr-16 font-thin text-xs w-[200px] ">
+                    Genomac Institute Inc.
+                    <p className='-ml-2' >| USA Incorporated</p>
+                  </p>
                 </div>
-            </div>
 
-            <div className="flex justify-center -mt-9">
-                <button
-                    className="bg-purple-600 p-2 rounded-xl hover:bg-purple-700 text-white"
-                    onClick={onButtonClick}
-                >
-                    Download Certificate
-                </button>
+                <div className="text-center mx-auto">
+                  <p className="uppercase font-semibold text-3xl ">
+                    certificate of learning
+                  </p>
+                  <p className="text-center italic font-bold ">
+                    this certificate is rewarded to:
+                  </p>
+                </div>
+
+                <div className=" text-center mx-auto pt-5 pb-10 w-[1000px] h-[200px] mt-5 ">
+                  <p className="text-3xl font-semibold border-b-2 mx-[300px] pb-2 mb-1 border-purple-800 border-dashed ">
+                    {" "}
+                    {formData.name}{" "}
+                  </p>
+                  <p>
+                    for demonstrating active learning in the short course on
+                  </p>
+                  <p className="font-bold text-purple-800 capitalize text-lg py-3 ">
+                    Applied functional genomics and bioinformatics
+                  </p>
+                  <p className="mx-20 ">
+                    This fully funded short course covers an in-depth hands-on
+                    exploration of functional genomics focusing on genome
+                    annotation and functional discovery across plant, animal and
+                    human genomes by leveraging bioinformatics tools for
+                    comprehensive genomic analysis, enabling extraction of
+                    meaningful insights from raw genomic data.
+                  </p>
+                  <p className="font-bold uppercase"> {formattedDate} </p>
+                </div>
+
+                <div className="flex justify-between mx-32 ">
+                  <div className=" ">
+                    <p className="border-b-2 border-dashed border-purple-800 w-[200px] ">
+                      <img
+                        src={sign1}
+                        alt="signature"
+                        className="w-[200px] h-[150px] -mb-10 "
+                      />
+                    </p>
+                    <p className="text-base font-semibold">
+                      Oluwaseyi Abraham Olawale
+                    </p>
+                    <p className="text-xs font-medium">
+                      Founder & CEO of Genomac Holdings.
+                    </p>
+                  </div>
+
+                  <div className="w-[400px] h-auto -mt-[30px] -ml-[400px] -mr-[320px] z-10">
+                    <img src={award} alt="award" />
+                  </div>
+
+                  <div className="mt-2 ">
+                    <p className="border-b-2 border-dashed border-purple-800 w-52 ">
+                      <img
+                        src={sign2}
+                        alt="signature"
+                        className="w-[200px] h-[150px] -mb-12 "
+                      />
+                    </p>
+                    <p className="text-base font-semibold">
+                      Oluwaseun Oyekunle Agboola
+                    </p>
+                    <p className="text-xs font-medium">
+                      Director, Genomac Institute INC.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="w-[150px] absolute top-8 left-14 ">
+                  <img src={award1} alt="award" />
+                </div>
+              </div>
             </div>
-        </>
+          </div>
+        </div>
+
+        <div className="flex justify-center -mt-9">
+          <button
+            className="bg-purple-600 p-2 rounded-xl hover:bg-purple-700 text-white"
+            onClick={onButtonClick}
+          >
+            Download Certificate
+          </button>
+        </div>
+      </>
     );
 }
 
